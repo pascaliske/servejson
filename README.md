41118b719e74310cde1676012b739a22f6250add
Pascal Iske <info@pascal-iske.de>
2018-01-08 19:56:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# `@pascaliske/servejson`
+
+[![npm](https://img.shields.io/npm/v/@pascaliske/servejson.svg?style=flat)](https://www.npmjs.com/package/@pascaliske/servejson)
+[![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg?style=flat)](https://github.com/pascaliske/servejson/blob/master/LICENSE.md)
+[![Travis](https://img.shields.io/travis/pascaliske/servejson.svg?style=flat)](https://travis-ci.org/pascaliske/servejson)
+[![Greenkeeper badge](https://badges.greenkeeper.io/pascaliske/servejson.svg)](https://greenkeeper.io/)
+
+> Command line utility for quickly serving a given json file with correct http headers.
+
+## Install
+
+```bash
+$ yarn global add @pascaliske/servejson
+```
+
+## CLI
+
+Use the globally installed cli to quickly open up a servejson:
+
+```bash
+$ servejson <path/to/file.json> [<custom-port>]
+```
+
+## License
+
+MIT © [Pascal Iske](https://pascal-iske.de)
---
+++ src/cli.ts
@@ -0,0 +1,38 @@
+import * as pkg from '../package.json'
+import { Logger, Arguments } from 'modern-cli'
+import { resolve } from 'path'
+import * as express from 'express'
+import * as cors from 'cors'
+
+// fetch package data
+const { name, version } = (<any>pkg)
+
+// enable logging
+process.env.DEBUG = name
+
+const log: Logger = new Logger(name)
+const args: Arguments = new Arguments()
+const file: string = args.get(0) || false
+const port: number = args.get(1) || 9001
+
+// log version
+log(`v${version}`)
+
+// check given url
+if (!file) {
+    log.red('Please enter a file to serve!')
+    process.exit(1)
+}
+
+// create express server
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req: express.Request, res: express.Response) => {
+    res.sendFile(resolve(file))
+})
+
+app.listen(port, () => {
+    log(`serving ${Logger.cyan(file)} at ${Logger.cyan(`http://localhost:${port}`)}`)
+})
